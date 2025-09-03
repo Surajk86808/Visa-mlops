2b34478aae6e914452e5383a4890f130c182d96c
suraj <surajk86808@gmail.com>
2025-09-04 03:49:22 +0530

    Clean repository

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="Visa",
-    version="0.0.0",
-    author="Suraj",
-    author_email="Surajk86808@gmail.com",
-    packages=find_packages()
-)
