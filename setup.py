ac61ccd64a151321b41207f5c03b5bf1fbf63430
suraj <surajk86808@gmail.com>
2025-09-04 01:02:37 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Visa",
+    version="0.0.0",
+    author="Suraj",
+    author_email="Surajk86808@gmail.com",
+    packages=find_packages()
+)
